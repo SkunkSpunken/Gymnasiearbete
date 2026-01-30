0fa2251d1da13ccdaf20e99457bc6c433106a958
SkunkSpunken <theo.kjellsson@gmail.com>
2026-01-30 09:29:24 +0100

    FIxa pause menu så att den fungerar

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -2,20 +2,35 @@ extends Control
 
 func _ready() -> void:
 	visible = false
-	get_tree().paused = false
+	
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
-		if get_tree().paused:
-			visible = false
-			get_tree().paused = false
-		else:
-			visible = true
-			get_tree().paused = true
+func _resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("Fade in")
+
+
+func _pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("Fade in")
+
+
+func _esc():
+	if Input.is_action_just_pressed("Escape") and !get_tree().paused:
+		visible = true
+		_pause()
+	elif Input.is_action_just_pressed("Escape") and get_tree().paused:
+		visible = false
+		_resume()
+
+
 
 func _on_resume_pressed() -> void:
-	visible = false
-	get_tree().paused = false
+	_resume()
+
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+
+func _process(_delta):
+	_esc()
